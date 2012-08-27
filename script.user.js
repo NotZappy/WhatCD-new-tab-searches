ab8a2cc0f646a8ec7de2846054856af1fa3dabe3
Z4ppy <Z4ppy@server.fake>
2012-08-27 00:54:58 +0000

    Fix @include URL to use SSL (transferred from userscripts.org)

--- script.user.js
+++ script.user.js
@@ -3,12 +3,18 @@
 // @namespace      What.CD
 // @description    Open a new tab when searching something through the searchbar.
 // @author         Z4ppy
-// @include        http://what.cd/*
+// @include        https://what.cd/*
 // @include        https://ssl.what.cd/*
-// @updateURL      http://userscripts.org/scripts/source/123202.user.js
+// @updateURL      http://userscripts.org/scripts/source/123195.meta.js
 // @version        1.0
-// @date           2012-01-15
+// @date           2012-08-27
 // ==/UserScript==
+
+/*
+Changelog:
+1.0    2012-01-15  Initial version
+1.0.1  2012-08-27  Fix @include for https://what.cd
+*/
 
 (function() {
 	var forms = document.getElementsByTagName('form');
