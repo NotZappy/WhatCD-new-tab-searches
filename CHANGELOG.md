0cd052fd8d5c11bb7381a822109ce706a2274a8c
Z4ppy <Z4ppy@server.fake>
2014-07-21 14:21:30 +0200

    Getting ready for Github and cosmetic updates

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+**1.0** (2012-01-15)
+Initial version
+
+**1.0.1** (2012-08-27)
+Fix @include for https://what.cd
+
+**1.1** (2013-03-09)
+Rewritten
--- script.user.js
+++ script.user.js
@@ -1,21 +1,13 @@
 // ==UserScript==
-// @name           What.CD Search in new Tab
-// @namespace      What.CD
+// @name           What.CD :: Search to New Tab
+// @namespace      Z4ppy.What.CD
 // @description    Open a new tab when searching something through the searchbar.
 // @author         Z4ppy
-// @include        https://what.cd/*
-// @include        https://ssl.what.cd/*
+// @include        https://*what.cd/*
 // @grant          none
-// @version        1.1
-// @date           2013-03-09
+// @version        1.1.1
+// @date           2014-07-21
 // ==/UserScript==
-
-/*
-Changelog:
-1.0    2012-01-15  Initial version
-1.0.1  2012-08-27  Fix @include for https://what.cd
-1.1    2013-03-09  Rewritten
-*/
 
 NodeList.prototype.forEach = Array.prototype.forEach;
 var forms = document.querySelectorAll('#searchbars form');
